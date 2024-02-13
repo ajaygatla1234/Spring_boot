95d32b3ad1b5f5672b7d29719e005de7c06c1571
ajaygatla1234 <149674849+ajaygatla1234@users.noreply.github.com>
2024-02-13 17:03:00 +0530

    Add files via upload

---
+++ 1. MyPractice_springboot-actuator_1/src/main/java/com/gl/springboot/controller/HelloController.java
@@ -0,0 +1,35 @@
+package com.gl.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Annotation to indicate that this class serves as a RESTful controller
+@RestController
+public class HelloController {
+
+    // This method handles HTTP GET requests for the "/hello" endpoint
+    @GetMapping("hello")
+    public String hello() {
+        // Returns a simple "Hello...Motttooo..." response
+        return "Hello...Motttooo...";
+    }
+}
+
+
+
+/*
+ * Execution links for this code
+ * http://localhost:8087/hello
+ * http://localhost:8087/actuator/
+ * http://localhost:8087/actuator/info
+ * http://localhost:8087/actuator/health
+ * 
+ * for customized path
+ * http://localhost:8087/app/health
+ * .
+ * .
+ * .so on
+ * */
